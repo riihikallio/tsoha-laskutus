87c30a86e2e613ced0ffcbb7d23d4cbba408e53c
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-02-01 00:17:59 +0200

    Change category to drop-down

--- application/product/forms.py
+++ application/product/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, validators
+from wtforms import StringField, FloatField, SelectField, validators
 
 class ProductForm(FlaskForm):
     name = StringField("Name", [validators.InputRequired()])
     unit = StringField("Unit", [validators.InputRequired()])
     price = FloatField("Price", [validators.NumberRange(min=0.01)])
-    category = StringField("Category", [validators.InputRequired()])
+    category = SelectField("Category", choices=[('Laitteet', 'Laitteet'), ('Osat', 'Osat'), ('Palvelut', 'Palvelut')])
 
     class Meta:
         csrf = False
