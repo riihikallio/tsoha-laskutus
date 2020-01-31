cc5c2e12d6384a02d627946c18d3df11e948e8c6
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-01-31 22:39:26 +0200

    Prepare for Heroku

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///invoice.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
