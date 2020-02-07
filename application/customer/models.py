634e63dfff8eeb86c7c2d7af603f512b8271d0e6
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-02-07 21:42:18 +0200

    Add relationships

--- application/customer/models.py
+++ application/customer/models.py
@@ -9,6 +9,8 @@ class Customer(db.Model):
     name = db.Column(db.String(144), nullable=False)
     address = db.Column(db.String(255))
 
+    invoices = db.relationship("Invoice", backref='customer', lazy=True)
+
     def __init__(self, name, address):
         self.name = name
         self.address = address
