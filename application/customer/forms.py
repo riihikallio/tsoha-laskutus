21d089ae2cd0be8f0f12a03e32dd36561ce8f6aa
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-02-20 21:33:36 +0200

    Fix address length validation

--- application/customer/forms.py
+++ application/customer/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField, validators
 
 class CustomerForm(FlaskForm):
     name = StringField("Name", [validators.Length(min=3,max=144)])
-    address = TextAreaField("Address", [validators.Length(min=3,max=144)])
+    address = TextAreaField("Address", [validators.Length(min=3,max=255)])
  
     class Meta:
         csrf = False
