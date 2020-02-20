71b3f37363b38c9a15a05a06954bcbe94929d4dd
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-02-20 22:30:01 +0200

    Fix reporting with a HAVING clause

--- application/reports/models.py
+++ application/reports/models.py
@@ -7,6 +7,7 @@ def tabulate(res):
   result = []
   
   for row in res:
+    amount = 0 if row[2]==None else row[2]
     if prev==None:  # First row
       prev = row[0]
       result.append([row[0], "", ""])
@@ -15,9 +16,9 @@ def tabulate(res):
       total = 0
       prev = row[0]
       result.append([row[0], "", ""])
-    result.append(["", row[1], '{:.2f}€'.format(row[2])])
-    total += row[2]
-    grand += row[2]
+    result.append(["", row[1], '{:.2f}€'.format(amount)])
+    total += amount
+    grand += amount
 
   result.append(["", "Total:", '{:.2f}€'.format(total)])
   result.append(["Grand total:", "", '{:.2f}€'.format(grand)])  
@@ -25,22 +26,24 @@ def tabulate(res):
 
 
 def sales_by_category():
-  stmt = "SELECT Product.category, Account.username, SUM(Product.price*Row.qty) FROM Product"
+  stmt = "SELECT Product.category, Account.name, SUM(Product.price*Row.qty) as Total FROM Product"
   stmt += " LEFT JOIN Row ON Product.number = Row.product_num"
   stmt += " LEFT JOIN Invoice ON Invoice.number = Row.invoice_num"
   stmt += " LEFT JOIN Account ON Account.id = Invoice.account_id"
   stmt += " GROUP BY Product.category, Account.id"
+  stmt += " HAVING Total > 0"
   stmt += " ORDER BY Product.category, Account.username"
 
   return tabulate(db.engine.execute(stmt))
 
 
 def sales_by_customer():
-  stmt = "SELECT Customer.name, Product.category, SUM(Product.price*Row.qty) FROM Customer"
+  stmt = "SELECT Customer.name, Product.category, SUM(Product.price*Row.qty) as Total FROM Customer"
   stmt += " LEFT JOIN Invoice ON Invoice.customer_num = Customer.number"
   stmt += " LEFT JOIN Row ON Row.invoice_num = Invoice.number"
   stmt += " LEFT JOIN Product ON Product.number = Row.product_num"
   stmt += " GROUP BY Customer.name, Product.category"
+  stmt += " HAVING Total > 0"
   stmt += " ORDER BY Customer.name, Product.category"
 
   return tabulate(db.engine.execute(stmt))
